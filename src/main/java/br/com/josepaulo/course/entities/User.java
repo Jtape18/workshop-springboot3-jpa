24f25b5c07fbd54c5c39e7b7645071a0aa4fa329
José <josepaulo1607@gmail.com>
2025-07-04 14:42:04 -0300

    Order, Instant, ISO 8601

--- src/main/java/br/com/josepaulo/course/entities/User.java
+++ src/main/java/br/com/josepaulo/course/entities/User.java
@@ -1,15 +1,21 @@
 package br.com.josepaulo.course.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 
 @Data
@@ -29,4 +35,9 @@ public class User implements Serializable {
     private String name;
     private String phone;
     private String password;
+
+    @Getter
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 }
